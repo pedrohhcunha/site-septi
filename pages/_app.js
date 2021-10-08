9ffcc5877c3888dcd4b8a7a99cceb5760d73797c
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-08 17:44:06 -0300

    Comentários e refatoração no _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+//Definido o app -> Primeiro arquivo que será executado
+
+//Importando o arquivo de estilização global
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
+
+//Criando e exportanto o componente inicial da aplicação
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
