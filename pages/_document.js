d744bf5877ae2eb693bde7162a81162749190406
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-08 11:30:33 -0300

    omeando a função Document

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
-export default () => {
+export default function Document () {
     return (
         <Html lang="pt-br">
             <Head />
