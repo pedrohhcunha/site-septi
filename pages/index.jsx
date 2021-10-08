8a753c64ab5a6f9066f71ce96155fa993315da3f
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-08 17:27:08 -0300

    Comentários e refatoração da página Inicial

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,45 @@
+
+//Importando o modulo para a estilização da página
+import styles from '../styles/Home.module.scss'
+
+//IMportando componetentes utilizados na página
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import FacaPedido from '../components/FacaPedido'
 import Clientes from '../components/Clientes'
 import CardSaibaMais from '../components/CardSaibaMais'
-import styles from '../styles/Home.module.scss'
 import Diferenciais from '../components/Diferenciais'
 import Button from '../components/Button'
-import placeholder from '../public/images/profile.png'
 import Head from 'next/head'
 
+//Importando as imagens utilizadas na página
+import placeholder from '../public/images/profile.png'
+
+//Criando o componente da página inicial
 export default function Home() {
+
+  const abrirFormulário = (idForm) => {
+    console.log("Abrindo form:", idForm)
+  }
+
+  //Retornando o JSX da página Inicial
   return (
     <>
+
+      {/* Definindo as metatags da página */}
       <Head>
         <title>A SEPTI | SEPTI Healthcare</title>
         <meta name="description" content="A septi é uma industria onde fabricamos e comercializamos EPIs, descartáveis e hospitalares, voltados para a paramentação segura em procedimentos cirúrgicos." />
       </Head>
+
+      {/* Instanciando o componente do Header */}
       <Header />
+
+      {/* Criando a seção inicial do site */}
       <section className={styles.sectionInicio}>
         <div className={styles.contentInicio}>
           <h1 className="fonteBranca">
-            Septi é <span className="fonteCoral">+ </span> <br/>
+            Septi é <span className="fonteCoral">+</span> <br/>
             qualidade <br/>
             <span className="fonteCoral"> segurança</span>
           </h1>
@@ -28,10 +47,12 @@ export default function Home() {
             buttonHover
             sizeButton="large"
             typeButton="principal"
-            actionButton={() => console.log("Hello world")}
+            actionButton={() => abrirFormulário(1)}
           >Conheça nossos produtos </Button>
         </div>
       </section>
+    
+      {/* Criando a seção do video */}
       <section className={styles.sectionVideo}>
         <iframe className={styles.iframe} src="https://www.youtube.com/embed/_ryi9ibyG18" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
         <div className={styles.content}>
@@ -41,33 +62,89 @@ export default function Home() {
             buttonHover
             sizeButton="large"
             typeButton="principal"
-            actionButton={() => console.log("Hello world")}
+            actionButton={() => abrirFormulário(1)}
           >Conheça nossos produtos </Button>
         </div>
       </section>
+
+      {/* Instanciando o componente de Diferenciais */}
       <Diferenciais />
+
+      {/* Criando a seção de produtos */}
       <section className={styles.sectionProdutos}>
         <h2 className="fonteAzul">Nossas soluções</h2>
         <div className={styles.produtos}>
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos" title="Aventais para #b pacientes #b" image={placeholder} />
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos" title="Aventais para #b isolamento #b" image={placeholder} />
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos" title="Aveitais #b barreira #b" image={placeholder} />
-
-          <CardSaibaMais alternative alt="Placeholder da imagem de linha de produtos" title="Aventais #b laminados #b" image={placeholder} />
-          <CardSaibaMais alternative alt="Placeholder da imagem de linha de produtos" title="Aventais de #b cirúrgicos #b" image={placeholder} />
-          <CardSaibaMais alternative alt="Placeholder da imagem de linha de produtos" title="#b Máscaras #b" image={placeholder} />
-          
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos" title="Panos e #b Wipers #b" image={placeholder} />
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos" title="Campos e #b invólucros #b" image={placeholder} />
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos"  title="Toucas e #b propés #b" image={placeholder} />
-
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos" alternative title="Macacões e #b conjuntos #b" image={placeholder} />
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos" alternative title="#b Papéis #b" image={placeholder} />
-          <CardSaibaMais alt="Placeholder da imagem de linha de produtos"  alternative title="Correlatos em #b TNT #b" image={placeholder} />
+          <CardSaibaMais
+            alt="Aventais para pacientes SEPTI SEPTI HEALTHCARE"
+            title="Aventais para #b pacientes #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alt="Aventais para isolamento SEPTI HEALTHCARE"
+            title="Aventais para #b isolamento #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alt="Aventais de barreira SEPTI HEALTHCARE"
+            title="Aveitais #b barreira #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alternative
+            alt="Aventais laminados SEPTI HEALTHCARE"
+            title="Aventais #b laminados #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alternative alt="Aventais cirúrgicos SEPTI HEALTHCARE"
+            title="Aventais de #b cirúrgicos #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alternative alt="Linha de máscaras SEPTI HEALTHCARE"
+            title="#b Máscaras #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alt="Panos e Wipers SEPTI HEALTHCARE"
+            title="Panos e #b Wipers #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alt="Campos e invólucros SEPTI HEALTHCARE"
+            title="Campos e #b invólucros #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alt="Toucas e propés SEPTI HEALTHCARE" 
+            title="Toucas e #b propés #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alternative alt="Macacões e conjuntos SEPTI HEALTHCARE"
+            title="Macacões e #b conjuntos #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alternative alt="Linha de papéis SEPTI HEALTHCARE"
+            title="#b Papéis #b"
+            image={placeholder}
+          />
+          <CardSaibaMais
+            alternative alt="Correlatos em TNT SEPTI HEALTHCARE" 
+            title="Correlatos em #b TNT #b"
+            image={placeholder}
+          />
         </div>
       </section>
+
+      {/* Instanciando o componente de Clientes */}
       <Clientes />
+
+      {/* Instanciando o componente de Faça Seu Pedido */}
       <FacaPedido />
+
+      {/* Instanciando o componente de Rodapé */}
       <Footer />
     </>
   )
