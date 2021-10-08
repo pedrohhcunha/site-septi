01e236ff8feb333a765685e1f2332c2e242684bc
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-08 10:13:22 -0300

    Corrigindo erro gramatical no titulo

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
     <Head>
-      <title>A SEPTI | SEPTI Heakthcare</title>
+      <title>A SEPTI | SEPTI Healthcare</title>
       <meta name="description" content="
         A septi é uma industria onde fabricamos e comercializamos EPIs, descartáveis e hospitalares, voltados para a paramentação segura em procedimentos cirúrgicos.
       " />
