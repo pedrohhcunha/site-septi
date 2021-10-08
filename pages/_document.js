f0edc7abf5559c88e8277077a7bca801c467c607
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-08 17:41:27 -0300

    Comentários e refatoração na criação do _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,15 @@
+//Criação de um novo templete de document para a inserção da linguagem do html
+
+//Importando os componentes necessários
 import { Head, Html, Main, NextScript } from 'next/document'
 
+//Criando o componente
 export default function Document () {
+
+    //Retornando o JSX do componente
     return (
+
+        //Definindo a linguagem do Html e recriando a estrutura padrão
         <Html lang="pt-br">
             <Head />
             <body>
