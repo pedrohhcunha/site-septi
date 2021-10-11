e0698bc9f2f685336c29d95a00a285e989830ca2
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-11 10:29:21 -0300

    Comnetários e refatoração do componente Clientes

--- components/Clientes.jsx
+++ components/Clientes.jsx
@@ -1,4 +1,9 @@
+//Criando o componente para a seção de clientes
+
+//Importando modulo para estilização
 import styles from '../styles/Clientes.module.scss'
+
+//Importando imagens necessárias
 import Image from 'next/image'
 import Cliente1 from '../public/images/cliente1.png'
 import Cliente2 from '../public/images/cliente2.png'
@@ -6,24 +11,25 @@ import Cliente3 from '../public/images/cliente3.png'
 import Cliente4 from '../public/images/cliente4.png'
 import Cliente5 from '../public/images/cliente5.png'
 
-// Import Swiper React components
+//Importando os componentes para a criação do slide
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from 'swiper';
+
+//Importando a estilização do slider
 import "swiper/css";
 import "swiper/css/pagination"
 
-// import Swiper core and required modules
-import SwiperCore, {
-  Autoplay
-} from 'swiper';
-
-// install Swiper modules
+//Instalando os modulos do swipper na aplicação
 SwiperCore.use([Autoplay]);
 
+//Definindo e exportando o componente
 export default function Clientes(){
   return (
     <section className={styles.section}>
       <h2 className="fonteBranca">Quem confia na gente</h2>
       <h3 className={styles.subTitulo}>Mais de 4.000 empresa já confiam na gente.</h3>
+
+      {/* Instanciando o slider de clientes */}
       <Swiper
         breakpoints={{
           200: {
@@ -90,6 +96,8 @@ export default function Clientes(){
           <Image priority className={styles.image} alt="Imagem de parceiro chave" src={Cliente5} />
         </SwiperSlide>
       </Swiper>
+
+      {/* Estruturando componentes para fazer fade in e fade out no slide */}
       <div className={styles.leftBlur}></div>
       <div className={styles.rightBlur}></div>
     </section>
