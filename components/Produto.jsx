8b8c85bab99fbdbc69502e72e37d66c33c754627
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-14 08:31:36 -0300

    Comentários e redatoração do componente Produtos

--- components/Produto.jsx
+++ components/Produto.jsx
@@ -1,13 +1,45 @@
+//Componente de seção para ser utilizado nas páginas de produto
+//alternative: <boolean> | Define a tipagem de estilização do elemento
+//title: <string> | Define o titulo da seção do produto
+//description: <string> | Define a descrição do produto
+//images: <list[url]> | Define as imagens que representaram o produto
+
+//Importando módulo para a estilização do componente
 import styles from '../styles/Produto.module.scss'
+
+//Importando os componentes necessários
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import Button from './Button'
+
+//Importando os hooks necessários
 import { useState } from 'react'
 
+//Definindo e exortanto o componente
 export default function Produto(props) {
 
+    //Criando controlador do estado atual do carrosel- representa um index de props.images
     const [stateCarossel, setStateCarossel] = useState(0);
 
+    //Definindo função para retroceder uma posição no carrossel
+    const prevCarrossel = () => {
+        let futureState = 0
+        if(stateCarossel >= 1){
+            futureState = stateCarossel - 1
+        }
+        setStateCarossel(futureState)
+    }
+
+    //Definindo função para avançar uma posição no carrossel
+    const nextCarrosel = () => {
+        let futureState = props.images.length - 1
+        if(stateCarossel < props.images.length - 1){
+            futureState = stateCarossel + 1
+        }
+        setStateCarossel(futureState)
+    }
+
+    //Retornando o JSX do componente
     return (
         <section className={`${styles.sectionProduto} ${props.alternative ? styles.alternative : ''}`}>
             <div className={styles.areaContent}>
@@ -47,8 +79,16 @@ export default function Produto(props) {
                         <div className={`${styles.pageSwipper} ${index === stateCarossel ? styles.activePage : ''}`}></div>
                     ))}
                 </div>
-                <FontAwesomeIcon className={styles.prevSwipper} icon={faAngleLeft} />
-                <FontAwesomeIcon className={styles.nextSwipper} icon={faAngleRight} />
+                <FontAwesomeIcon
+                    className={styles.prevSwipper}
+                    icon={faAngleLeft}
+                    onClick={prevCarrossel}
+                />
+                <FontAwesomeIcon
+                    className={styles.nextSwipper}
+                    icon={faAngleRight}
+                    onClick={nextCarrosel}
+                />
             </div>
         </section>
     ) 
