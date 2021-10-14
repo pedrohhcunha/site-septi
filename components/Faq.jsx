4ea379a8a6e969aae7b047486bc98e44d7fd7ddb
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-14 08:44:51 -0300

    Comentários e refatoração do componente de faq

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -1,12 +1,22 @@
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react';
+//Definindo componente de faq a ser utilizados nas páginas de linhas de produto
+
+//Importando o módulo para a estilização do componente
 import styles from '../styles/Faq.module.scss'
 
+//Importando os componentes necessários
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+//Importando os hooks necessários
+import { useState } from 'react';
+
+//Definindo e exportanto o componente
 export default function Faq(props) {
 
+  //Definindo controlador para definir qualn das perguntas estará com a resposta visivél
   const [idOpen, setIdOpen] = useState(0);
   
+  //Exportando o JSX do componente
   return(
     <section className={styles.faq}>
       <h2 className="fonteAzul">Perguntas Frequentes</h2>
