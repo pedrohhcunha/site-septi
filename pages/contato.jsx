21bdd2edc6f756bf2bc6d282c640a25b4739eeb0
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-20 14:49:20 -0300

    CU-1pcc7yz - Página Contato

--- pages/contato.jsx
+++ pages/contato.jsx
@@ -7,7 +7,24 @@ export default function Contato(props) {
         <main className={styles.main}>
             <Header />
             <section className={styles.section}>
-                CONTATO
+                <h2>CONTATO</h2>
+                <div className={styles.content}>
+                    <div className={styles.contatoForm}>
+                        <h3>Formulário</h3>
+                    </div>
+                    <div className={styles.contatoInfo}>
+                        <h3>Contato</h3>
+                        <span>
+                        Azeplast Indústria e Comércio Ltda <br/>
+                        Rua Beira Rio, 215 E, Bairro Efapi <br/>
+                        Chapecó/SC. CEP: 89809-807 <br/>
+                        <br/>
+                        (49) 3328.6515 <br/>
+                        azeplast@azeplast.com.br <br/>
+                        comercial@azeplast.com.br <br/>
+                        </span>
+                    </div>
+                </div>
             </section>
             <Footer />
         </main>
