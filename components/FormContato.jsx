3ddbc8d05519bcc1d3d70444ab3979a73503680c
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-21 15:53:51 -0300

    CU-1pcfvuu - Criar comunicação entre o formulário de contato e o backend

--- components/FormContato.jsx
+++ components/FormContato.jsx
@@ -3,8 +3,11 @@ import Input from './Input'
 import Textarea from './Textarea'
 import CheckboxList from './CheckboxList'
 import { useState } from 'react'
+import axios from 'axios'
 
 export default function FormContato(props){
+
+    const [isSending, setIsSending] = useState(false);
     
     const itensCheckboxList = [
         "Aventais para procedimentos",
@@ -54,18 +57,32 @@ export default function FormContato(props){
     }
 
     const submitForm = (event) => {
-        event.preventDefault()
+        setIsSending(true)
         let produto_interesse = []
         contatoData.produto_interesse.map(id => {
             produto_interesse.push(itensCheckboxList[id])
         })
         contatoData.produto_interesse = produto_interesse
 
+        axios.post(
+            process.env.NEXT_PUBLIC_LINK + '/api/forms/contato',
+            contatoData
+        ).then(response => {
+            setIsSending(false)
+            if(response.data.msg === "success"){
+                window.location.href = `${process.env.NEXT_PUBLIC_LINK}/obrigado`
+            } else {
+                console.log('erro')
+            }
+        })
+
         console.log(contatoData)
+        document.querySelector('#FormContato').reset()
+        event.preventDefault()
     }
 
     return(
-        <form onSubmit={() => submitForm(event)} className={styles.form}>
+        <form id="FormContato" onSubmit={() => submitForm(event)} action="" method="POST" className={`${styles.form} ${isSending ? styles.sending : ''}`}>
             <h3>Entre em contato</h3>
             <Input changeFunction={handlerInputs} required name="nome" label="Nome" type="text" />
             <Input changeFunction={handlerInputs} required name="email" label="Email" type="email" />
@@ -78,6 +95,7 @@ export default function FormContato(props){
             <Textarea changeFunction={handlerInputs} required name="observacoes" label="Observações" />
             <button type="submit" className={styles.button}>Enviar Agora</button>
             <span>A septi é contra qualquer tipo de span, desta forma não usaremos suas informações de contato para isso.</span>
+            <div className={styles.sendDiv}></div>
         </form>
     )
 }
