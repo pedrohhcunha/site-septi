b277a34cf0aff2fd654a9aaada8fb57e3c38a630
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-21 13:33:19 -0300

    Corrigindo Build errors

--- components/FormContato.jsx
+++ components/FormContato.jsx
@@ -2,7 +2,9 @@ import styles from '../styles/FormContato.module.scss'
 import Input from './Input'
 import Textarea from './Textarea'
 import CheckboxList from './CheckboxList'
-export default function (){
+
+export default function FormContato(props){
+    
     const itensCheckboxList = [
         "Aventais para procedimentos",
         "Aventais cirúrgicos",
@@ -20,15 +22,15 @@ export default function (){
     return(
         <form action="" className={styles.form}>
             <h3>Entre em contato</h3>
-            <Input label="nomeContato" label="Nome" type="text" />
-            <Input label="emailContato" label="Email" type="email" />
-            <Input label="empresaContato" label="Empresa" type="text" />
-            <Input label="cargoContato" label="Cargo" type="text" />
-            <Input label="enderecoContato" label="Endereço" type="text" />
-            <Input label="telefoneContato" label="Telefone" type="text" />
-            <Input label="cnpjContato" label="CNPJ /CPF" type="text" />
+            <Input name="nomeContato" label="Nome" type="text" />
+            <Input name="emailContato" label="Email" type="email" />
+            <Input name="empresaContato" label="Empresa" type="text" />
+            <Input name="cargoContato" label="Cargo" type="text" />
+            <Input name="enderecoContato" label="Endereço" type="text" />
+            <Input name="telefoneContato" label="Telefone" type="text" />
+            <Input name="cnpjContato" label="CNPJ /CPF" type="text" />
             <CheckboxList title="Produtos de interesse" items={itensCheckboxList}/>
-            <Textarea label="observacoesContato" label="Observações" />
+            <Textarea name="observacoesContato" label="Observações" />
             <span>A septi é contra qualquer tipo de span, desta forma não usaremos suas informações de contato para isso.</span>
         </form>
     )
