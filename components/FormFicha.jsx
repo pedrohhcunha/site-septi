0c0ec5945f73cc8f04307c3c94bffe969b117fb3
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-22 10:29:37 -0300

    Alteração do titulo do form de ficha

--- components/FormFicha.jsx
+++ components/FormFicha.jsx
@@ -45,7 +45,7 @@ export default function props(props) {
 
   return (
     <form id="FormFicha" onSubmit={() => submitForm(event)} action="" method="POST" className={`${styles.form} ${isSending ? styles.sending : ''}`}>
-      <h3>Receba ficha técnica</h3>
+      <h3>Receba a ficha técnica</h3>
       <Input
         changeFunction={handlerInputs}
         required name="nome"
