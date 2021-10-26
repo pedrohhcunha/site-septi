5657094d06ff39c1aaee56fa4cd6f8c3edeb46da
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-26 10:31:00 -0300

    CU-1pqw1ap - Fazer comentários e refatoração do código - FormCatalogo

--- components/FormCatalogo.jsx
+++ components/FormCatalogo.jsx
@@ -1,12 +1,22 @@
+//Criando componente de formulário para comunicação com o backend
+
+//Importando módulo para estilização do formulário
 import styles from '../styles/Form.module.scss'
-import Input from './Input';
-import { useState } from 'react';
+
+//Importando componente necessários
 import axios from 'axios'
+import Input from './Input';
 
-export default function FormCatalogo(props) {
+//Importando os Hooks necessários
+import { useState } from 'react';
 
+//Definindo e exportando o componente
+export default function FormCatalogo() {
+
+  //Estado que controla se o envio dos dados está sendo feito para a API
   const [isSending, setIsSending] = useState(false);
 
+  //Estado que armazena os dados coletados a serem enviados via API
   const [catalogoData, setCatalogoData] = useState({
     "nome": "",
     "email": "",
@@ -16,6 +26,7 @@ export default function FormCatalogo(props) {
     "cargo": ""
   });
 
+  //Função que atualiza o valor de catalagoData sempre que houver mudanças em algum input
   const handlerInputs = (event) => {
       setCatalogoData({
           ...catalogoData,
@@ -23,18 +34,24 @@ export default function FormCatalogo(props) {
       })
   }
 
+  //Função responsável por validar e enviar os dados para a API
   const submitForm = (event) => {
     event.preventDefault()
     setIsSending(true)
+
+    //Reseta os campos do formulário
     document.querySelector('#FormCatalogo').reset()
 
+    //Retira os caracteres do cnpj
     catalogoData.cpf_cnpj = catalogoData.cpf_cnpj.replace('/[^0-9]/', '')
 
+    //Envia os dados para a API
     axios.post(
         `${process.env.NEXT_PUBLIC_LINK}/api/forms/catalogo`,
         catalogoData
     ).then(response => {
         setIsSending(false)
+
         if(response.data.msg === "success"){
             window.location.href = `${process.env.NEXT_PUBLIC_LINK}/obrigado`
         } else {
