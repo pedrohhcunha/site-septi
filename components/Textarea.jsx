a700d13bddd24f3c4d2e93b60b99f4f3f7c60bc9
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-26 10:56:49 -0300

    CU-1pqw1ap - Fazer comentários e refatoração do código - Teaxtarea

--- components/Textarea.jsx
+++ components/Textarea.jsx
@@ -1,5 +1,13 @@
+//Componente de textarea estilizado e preparado para ser utilzado em formulários na aplicaçãos
+//label: <strgin> | Representa o titulo do textarea
+//name: <string> | Deve ser o mesmo nome que o do objeto que controla os dados do textarea
+//changeFunction: <function> Função a ser executada quando houver qualquer mudança no valor do textarea
+
+
+//Importando módulos para a estlização do componente
 import styles from '../styles/Textarea.module.scss'
 
+//Defindo e exportando o componente
 export default function Textarea(props) {
     return(
     <div className={styles.areainput}>
