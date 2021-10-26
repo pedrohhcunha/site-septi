1405cd1aed76f8e81f9fe882bc2a2176685943b3
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-26 10:14:31 -0300

    CU-1pqw1ap - Fazer comentários e refatoração do código - CheckboxList

--- components/CheckboxList.jsx
+++ components/CheckboxList.jsx
@@ -1,5 +1,12 @@
+//Componente de caixas de seleção para ser utilizado em formuário
+//Necessita de implmentação conjunta de função par manipular as ações exercidadas nos checklists
+//title: <string> | Define o titulo da lista
+//items: <list<string>> | Define as opções que estaráo presentes na lista
+
+//Importando a estilização necessário
 import styles from '../styles/CheckboxList.module.scss'
 
+//Definindo e exportando o componente
 export default function CheckboxList(props) {
     return(
         <>
