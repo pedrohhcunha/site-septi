9b954893e5ec077c9af30f4dc4c7de526f39f305
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-26 10:59:44 -0300

    CU-1pqw1ap - Fazer comentários e refatoração do código - Vaga

--- components/Vaga.jsx
+++ components/Vaga.jsx
@@ -1,9 +1,19 @@
-import styles from '../styles/Vaga.module.scss'
-import placeholder from '../public/images/profile.png'
-import Image from 'next/image'
-import DropDiv from './DropDIv'
-import Button from './Button'
+//Componente card de vaga a ser utilizado na tela de trablahe conosco
+//title: <string> | Define o titulo da vaga
+//responsabilidades: <string>   / Define as responsabilidade do possivel empregado
+//requisitos: <string>   / Define os requisitos do possivel empregado
+//oferecemos: <string>   / Explana os beneficios do possivel functionário
 
+//Importando módulo para a estilização do componente
+import styles from '../styles/Vaga.module.scss'
+
+//Importando componentes necessários
+import Button from './Button'
+import DropDiv from './DropDIv'
+import Image from 'next/image'
+import placeholder from '../public/images/profile.png'
+
+//Criando e exportando o componente
 export default function Vaga(props) {
 
     return (
