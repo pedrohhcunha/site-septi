e7dbf7a64f283395b8e6eebd8af2ef19c23c0d0a
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-10-26 11:05:58 -0300

    CU-1pqw1ap - Fazer comentários e refatoração do código - Remoove_spaces

--- utils/remove_spaces.js
+++ utils/remove_spaces.js
@@ -1,9 +1,17 @@
+//Exportando função que remove espaços em branco de uma string
+//stringWithSpaces: <string> | Sting para ser sanitizada
+
 export default function removeSpaces(stringWithSpaces) {
     let stringWithoutSpaces =  ''
+
+    //Itera sobre cada caracter de uma string
     for (let i = 0; i < stringWithSpaces.length; i++) {
         const letter = stringWithSpaces[i];
+
+        //Se o caractere for um espaço -> Substitui por nada | Senão -> Não faz nada
         stringWithoutSpaces += letter !== ' ' ? letter : '-'
     }
     
+    //Retorna a a string sem espaços
     return stringWithoutSpaces
 }
