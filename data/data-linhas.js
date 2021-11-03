6f2b955109feedd17dcd71e654b7b18d82efd3b8
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-03 11:22:47 -0300

    Adicionando conteúdos das linhas de produtos

--- data/data-linhas.js
+++ data/data-linhas.js
@@ -3,81 +3,89 @@
 module.exports = [
     {
     title: "Aventais para pacientes",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem est cum eaque velit cumque quos eius voluptas nobis veniam culpa ullam iusto, veritatis blanditiis ea nam deserunt itaque, reiciendis a.",
+      description: "Os aventais para pacientes Septi são indicados para o uso em atendimentos iniciais, procedimentos de rotina e exames clínicos em geral. Estão disponíveis em TNT SS (spunbond-spunbond) e TNT SMS (spunbond-meltblown-spunbond), em diferentes gramaturas e tamanhos. Conheça os detalhes de cada um.",
       image: '/linhas/teste1.png',
       produtos: [
         {
-          title: "titulo do produto",
-          tag: "FT_BALACLAVA",
-          description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nobis ex non cum, placeat delectus magni quo tempora sed et ut quidem, molestiae deleniti voluptatibus laborum laudantium cupiditate. Illo, iusto.",
+          title: "Avental para pacientes SS",
+          tag: "FT_APP_SS20G",
+          description: "O avental para pacientes SS Septi é produzido em TNT 100% polipropileno 20 ou 30 gramas (spunbond-spunbond) na cor branca. Possui amarração no verso, na região da cintura e pescoço e não contém mangas. Esta combinação de elementos garante mais conforto e praticidade aos pacientes.",
           images: [
             {
-              file: '/profile.png',
-              alt: 'desc da foto'
+              file: '/produtos/FT_APP_SS20G/AVENTAL_PACIENTES_SS_frente.png',
+              alt: 'Avental SS 20 gramas para pacientes. Avental SS 30 gramas para pacientes, sem manga. Camisola TNT branca.'
             },
             {
-              file: '/profile.png',
-              alt: 'desc da foto'
-            },
-            {
-              file: '/profile.png',
-              alt: 'desc da foto'
+              file: '/produtos/FT_APP_SS20G/AVENTAL_PACIENTES_SS.png',
+              alt: 'Avental SS 20 gramas para pacientes. Avental SS 30 gramas para pacientes, sem manga. Camisola TNT branca.'
             }
           ]
         },
         {
-          title: "titulo do produto",
-          tag: "FT_BALACLAVA",
-          description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nobis ex non cum, placeat delectus magni quo tempora sed et ut quidem, molestiae deleniti voluptatibus laborum laudantium cupiditate. Illo, iusto.",
+          title: "Avental para pacientes SMS",
+          tag: "FT_APP_SMS50G",
+          description: "Os aventais para pacientes SMS Septi são a combinação ideal entre proteção e conforto para os pacientes. Fabricados em tecido SMS (spunbond-meltblown-spunbond) 50 gramas, não possuem mangas, são respiráveis e ajudam a manter o corpo dos usuários quente em temperaturas baixas e a refrescá-los em altas temperaturas. Além disso, possuem a sua amarração no verso.",
           images: [
             {
-              file: '/profile.png',
-              alt: 'desc da foto'
+              file: '/produtos/FT_APP_SMS50G/AVENTAL_PACIENTES_SMS-A.png',
+              alt: 'Avental SS 50 gramas para pacientes, sem manga. Camisola TNT azul.'
             },
             {
-              file: '/profile.png',
-              alt: 'desc da foto'
+              file: '/produtos/FT_APP_SMS50G/AVENTAL_PACIENTES_SMS-B.jpg',
+              alt: 'Avental SS 50 gramas para pacientes, sem manga. Camisola TNT azul.'
             },
             {
-              file: '/profile.png',
-              alt: 'desc da foto'
+              file: '/produtos/FT_APP_SMS50G/AVENTAL_PACIENTES_SMS-C.png',
+              alt: 'Avental SS 50 gramas para pacientes, sem manga. Camisola TNT azul.'
+            },
+            {
+              file: '/produtos/FT_APP_SMS50G/AVENTAL_PACIENTES_SMS-D.png',
+              alt: 'Avental SS 50 gramas para pacientes, sem manga. Camisola TNT azul.'
+            },
+            {
+              file: '/produtos/FT_APP_SMS50G/AVENTAL_PACIENTES_SMS-E.png',
+              alt: 'Avental SS 50 gramas para pacientes, sem manga. Camisola TNT azul.'
             }
           ]
         }
       ],
       diferenciais: [
         {
-          title: "titulo do diferencial",
-          description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nobis ex non cum.",
+          title: "Mais leveza e conforto",
+          description: "A respirabilidade dos aventais TNT SS e TNT SMS evitam a retenção do calor corporal, proporcionando mais leveza e conforto aos usuários.",
           icon: "/profile.png"
         },
         {
-          title: "titulo do diferencial",
-          description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nobis ex non cum.",
+          title: "Privacidade adequada",
+          description: "Tanto o TNT SS quanto o TNT SMS apresentam gramaturas adequadas para uso em procedimentos de rotina, onde a transparência do tecido deve ser menor.",
           icon: "/profile.png"
         },
         {
-          title: "titulo do diferencial",
-          description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nobis ex non cum.",
+          title: "Resistência a rasgos e à tração",
+          description: "A matéria prima utilizada na construção dos Aventais Septi é a combinação perfeita entre fibras e polímeros que unidas garantem mais resistência nas rotinas hospitalares.",
           icon: "/profile.png"
         }
       ],
       perguntas: [
         {
-          pergunta: "a pergunta?",
-          resposta: "a resposta!"
+          pergunta: "Qual a vantagem de usar avental Septi?",
+          resposta: "Nossos aventais são pensados em cada detalhe, desde a escolha de materiais, o corte, o acabamento, a finalização, até a entrega para os usuários, a fim de proporcionar a máxima proteção com a melhor usabilidade às pessoas. Seguimos rigorosamente as legislações vigentes para a produção e comércio de Aventais (ABNT e ANVISA)."
         },
         {
-          pergunta: "a pergunta?",
-          resposta: "a resposta!"
+          pergunta: " Para que é indicado o avental para paciente?",
+          resposta: "Este avental é indicado para uso exclusivo em pacientes em procedimentos clínicos, laboratoriais ou cirúrgicos. Os aventais para pacientes Septi são ideais para exames de rotina e procedimentos ginecológicos ou urológicos."
         },
         {
-          pergunta: "a pergunta?",
-          resposta: "a resposta!"
+          pergunta: "Qual a gramatura deste avental?",
+          resposta: "Os aventais para pacientes SS estão disponíveis na gramatura 20g e 30g. Já o avental para pacientes SMS está disponível na gramatura 50g."
         },
         {
-          pergunta: "a pergunta?",
-          resposta: "a resposta!"
+          pergunta: "Como usar o avental para paciente?",
+          resposta: "Para utilizar o avental para pacientes SS Septi é muito simples, basta vestir com a abertura voltada para trás e fazer a amarração frontal na cintura ou no verso do pescoço."
+        },
+        {
+          pergunta: "Qual o pedido mínimo?",
+          resposta: "O pedido mínimo segue as políticas comerciais da Septi Healthcare e podem sofrer alterações de acordo com a região do país. Para mais informações clique aqui e fale com um consultor."
         },
       ]
     },
