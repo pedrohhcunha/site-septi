1e62b48a5898d2b4166c66d1d52f8fe45f9067ce
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-17 13:30:02 -0300

    permitindo imagens do intranet

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'septi.life', '192.168.30.71']
+    domains: ['localhost', 'septi.life', '192.168.30.71', 'api.onfinity.com.br']
   },
 }
